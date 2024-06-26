e4b5b68ab54e5953dd866853cc2ac367c4cc89ab
IzumiDKC <huudien111@gmail.com>
2024-06-27 00:44:21 +0700

    Áp dụng V9 + V8+ (Từ Duy) => V10. 1 số thay đổi...

--- src/main/java/Nhom08_AppleStore/repository/UserRepository.java
+++ src/main/java/Nhom08_AppleStore/repository/UserRepository.java
@@ -10,3 +10,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByPhone(String phone);
 }
+// khong dung den
